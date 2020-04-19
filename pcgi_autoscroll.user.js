6bc5918354af3818e8e1b4d3e9e22bf553f23cc6
MalikAQayum <l_malik_l@hotmail.com>
2020-04-19 18:21:22 +0200

    better page checking and autoscrolling.

--- pcgi_autoscroll.user.js
+++ pcgi_autoscroll.user.js
@@ -10,19 +10,41 @@
 // ==/UserScript==
 
 /* added this little script to autoscroll for stefan.
-** if need be you can increase/decrease, the variable maxscrolls to your needs.
+** if need be you can increase/decrease, the variables (big/small)maxscrolls to your needs.
 ** delaytimer should not be touched but if it is too slow then maybe decrease it to 3000.
+** keylog/curatorlog has a big page each > available; depending on the amount apps on each page (big/small)maxscrolls should be adujusted.
 */
-var maxscrolls = 75;
+var bigmaxscrolls = 75;
+var smallmaxscrolls = 15; 
 var delaytimer = 5000;
 var autoscrolling = 0;
-setInterval(function(){ 
-  if(autoscrolling < maxscrolls){
-    getData(); 
-    autoscrolling++;
-  }
-  else
+
+setInterval(function(){
+  var AutoScrollingPages = new RegExp(/pcgameit\.com\/(keylog|curatorlog)\/available\//); 
+  if(document.URL.match(AutoScrollingPages))
   {
-    /*donothing.*/
+    if(autoscrolling < bigmaxscrolls){
+      getData(); 
+      autoscrolling++;
+    }
+    else
+    {
+      /*donothing.*/
+    }
+  }
+}, delaytimer);
+
+setInterval(function(){
+  var AutoScrollingPages = new RegExp(/pcgameit\.com\/(keylog|curatorlog)\/(free|available|vr)\//); 
+  if(document.URL.match(AutoScrollingPages))
+  {
+    if(autoscrolling < smallmaxscrolls){
+      getData(); 
+      autoscrolling++;
+    }
+    else
+    {
+      /*donothing.*/
+    }
   }
 }, delaytimer);
