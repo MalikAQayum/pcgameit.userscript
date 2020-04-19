e8ccc58a23a338d8a0c24f4f1e8c7894b8c37ea0
MalikAQayum <l_malik_l@hotmail.com>
2020-04-19 18:07:23 +0200

    update v0.0.3
    made it a bit easier to be customized.

--- pcgi_autoscroll.user.js
+++ pcgi_autoscroll.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PCGameit.com autoscroll
 // @namespace    https://pcgameit.com
-// @version      0.0.2
+// @version      0.0.3
 // @description  autoscrolling for the pages that has a lot of data.
 // @author       MalikQayum
 // @connect      pcgameit.com
@@ -9,9 +9,13 @@
 // @require     https://code.jquery.com/jquery-3.4.1.min.js
 // ==/UserScript==
 
+/* added this little script to autoscroll for stefan.
+** if need be you can increase/decrease, the variable maxscrolls to your needs.
+** delaytimer should not be touched but if it is too slow then maybe decrease it to 3000.
+*/
 var maxscrolls = 75;
+var delaytimer = 5000;
 var autoscrolling = 0;
-
 setInterval(function(){ 
   if(autoscrolling < maxscrolls){
     getData(); 
@@ -21,4 +25,4 @@ setInterval(function(){
   {
     /*donothing.*/
   }
-}, 5000);
+}, delaytimer);
